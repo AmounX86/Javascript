6cb4a446a3759d880cfc8142bce177d59fbae91b
Mina Mounir Samuel <menax86@hotmail.com>
2022-07-14 21:37:10 +0200

    Create console_log.js

---
+++ console_log/console_log.js
@@ -0,0 +1,10 @@
+function simpleLog(){
+    console.log("Hello to developer console");
+}
+function logVariable(){
+    var x = 10;
+    console.log(x);
+}
+function logPassedVariable(x){
+    console.log(x);
+}
